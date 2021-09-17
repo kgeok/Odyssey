5057680593b5228d9b4e148a4ac56a607c380730
kgeok <{ID}+{username}@users.noreply.github.com>
2021-09-17 12:46:38 -0400

    (Commit on 'dev')

--- odyssey/lib/main.dart
+++ odyssey/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 // ignore: unused_import
 import 'dart:async';
@@ -17,7 +19,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(41.850033, -87.6500523);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -53,16 +55,18 @@ class _MyAppState extends State<MyApp> {
             // Important: Remove any padding from the ListView.
             padding: EdgeInsets.zero,
             children: [
-              const DrawerHeader(
+              DrawerHeader(
                 decoration: BoxDecoration(
                   color: Colors.blue,
                 ),
                 child: Text(
                   'Journal',
+                  style: GoogleFonts.quicksand(
+                      fontWeight: FontWeight.w700, fontSize: 22),
                 ),
               ),
               ListTile(
-                title: const Text('Item 1'),
+                title: const Text('Entry'),
                 onTap: () {
                   // Update the state of the app.
                   // ...
@@ -75,10 +79,20 @@ class _MyAppState extends State<MyApp> {
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
             target: _center,
-            zoom: 11.0,
+            zoom: 4.0,
           ),
         ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            // Add your onPressed code here!
+          },
+          icon: const Icon(Icons.push_pin),
+          label: Text('Actions',
+              style: GoogleFonts.quicksand(fontWeight: FontWeight.w700)),
+          backgroundColor: Colors.blue[600],
+        ),
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
